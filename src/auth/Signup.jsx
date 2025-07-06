b6e954a4bd844c7392d07f617ddf2349fb6e1fad
Eunice Asamoah <euniceasamoah399@gmail.com>
2025-07-06 16:03:04 +0000

    Adding form

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Signup = () => {
   return (
-    <div className='min-h-screen bg-cyan-900 '>
-<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-  <h2 className='text-center font-semibold text-3xl'>Signup</h2>
+    <div className=" bg-cyan-900 ">
+      <form action="" className="flex justify-center items-center min-h-screen ">
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <h2 className="text-center font-semibold text-3xl">Signup</h2>
 
-   <label className="label">Name</label>
-  <input type="email" className="input" placeholder="Name" />
-  
-  <label className="label">Email</label>
-  <input type="email" className="input" placeholder="Email" />
+          <label className="label">Name</label>
+          <input type="email" className="input" placeholder="Name" />
 
-  <label className="label">Password</label>
-  <input type="password" className="input" placeholder="Password" />
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
 
-  <button className="btn btn-neutral mt-4">Signup</button>
-</fieldset>
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
 
-
+          <button className="btn btn-neutral mt-4">Signup</button>
+        </fieldset>
+      </form>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
