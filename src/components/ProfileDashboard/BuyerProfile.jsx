3e689b895b0b2b0764f91e300e5f41f8954ba335
Nkunim Asaah Osei <oseinkunim@gmail.com>
2025-08-21 22:35:45 +0000

    adjustments

--- src/components/ProfileDashboard/BuyerProfile.jsx
+++ src/components/ProfileDashboard/BuyerProfile.jsx
@@ -3,11 +3,11 @@ import prof from '../../assets/images/avatar.jpg';
 
 const BuyerProfile = () => {
   return (
-    <div className="space-y-6 max-w-4xl mx-auto">
+    <div className="space-y-6 max-w-lg mx-auto">
       <h2 className="text-2xl font-semibold text-center text-gray-800">Buyer Profile</h2>
 
       {/* Profile Header */}
-      <div className="flex items-center justify-between p-6 bg-white rounded-2xl border border-gray-100 shadow-sm">
+      <div className="flex items-center justify-between p-6 rounded-2xl border border-gray-100 shadow-sm bg-white">
         <div className="flex items-center gap-4">
           <img src={prof} alt="Profile" className="w-16 h-16 object-cover rounded-full" />
           <div>
